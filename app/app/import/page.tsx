e2ab7ae2d65c49464eb985b88edb3ded4b5f4aaf
Diego Baca <diegofbacag@gmail.com>
2026-03-12 17:23:47 -0500

    feat(import): add import page

--- app/app/import/page.tsx
+++ app/app/import/page.tsx
@@ -1,23 +1,29 @@
 'use client'
 import { Dropzone } from '@/app/src/features/import/components/Dropzone'
-import { InvoiceIcon, ReceiptIcon } from '@phosphor-icons/react'
+import { BankIcon, InvoiceIcon } from '@phosphor-icons/react'
 
 export default function ImportPage() {
   return (
     <main className="bg-background p-8 max-w-5xl mx-auto w-full">
-      <h1 className="text-2xl font-bold mb-4">Importación de Datos</h1>
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Importación de Datos</h1>
+        <p className="text-slate-600 text-lg">
+          Sube tu reporte de ventas y tu estado de cuenta. Nuestro sistema
+          comparará ambos archivos automáticamente para detectar diferencias,
+          comisiones o pagos faltantes.
+        </p>
+      </div>
+
       <section className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-10">
         <Dropzone
           title="Reporte de PedidosYa"
-          description="Arrastra aquí tu CSV de pedidos o haz click para subir un archivo."
-          buttonLabel="Subir Reporte"
-          icon={ReceiptIcon}
+          description="Arrastra o sube el CSV exportado desde PedidosYa con el detalle de pedidos."
+          icon={InvoiceIcon}
         />
         <Dropzone
           title="Estado de Cuenta BCP"
-          description="Arrastra aquí tu CSV de pedidos o haz click para subir un archivo."
-          buttonLabel="Subir Estado de Cuenta"
-          icon={InvoiceIcon}
+          description="Arrastra aquí o haz click para subir el CSV de tu estado de cuenta del banco."
+          icon={BankIcon}
         />
       </section>
     </main>
--- app/src/features/import/components/Dropzone.tsx
+++ app/src/features/import/components/Dropzone.tsx
@@ -1,46 +1,82 @@
 'use client'
 
-import { Icon } from '@phosphor-icons/react'
-import { useState } from 'react'
+import { FilePlusIcon, Icon, UploadSimpleIcon } from '@phosphor-icons/react'
+import React, { useRef, useState } from 'react'
 
 interface DropzoneProps {
   title: string
   description: string
-  buttonLabel: string
   icon: Icon
 }
 
 export const Dropzone = ({
   title,
   description,
-  buttonLabel,
+
   icon: Icon,
 }: DropzoneProps) => {
-  const [fileName, setFileName] = useState(null)
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+
+    if (!file) return
+
+    setSelectedFile(file)
+  }
   return (
     <div className="flex flex-col">
       <div className="mb-2">
         {/* <p>Selecciona tu proveedor: PedidosYa</p> */}
       </div>
-      <div className="group flex flex-col items-center justify-center border-2 border-dashed border-[#e63746]/30 hover:border-[#e63746] bg-gray-100 rounded-xl p-10 transition-all cursor-pointer">
+      <div
+        className="group flex flex-col items-center justify-center border-2 border-dashed border-[#e63746]/30 hover:border-[#e63746] bg-white rounded-xl p-10 transition-all cursor-pointer"
+        onClick={() => inputRef.current?.click()}
+      >
         <div className="w-14 h-14 bg-[#e63746]/10 text-red-500 rounded-full flex items-center justify-center mb-4">
           <Icon size={28} />
         </div>
 
         <h3 className="text-lg font-bold text-slate-900">{title}</h3>
-        {fileName ? (
-          <p className="text-sm text-[#e63746] font-medium mt-2">{fileName}</p>
-        ) : (
-          <p className="text-sm text-slate-500 text-center mt-2 px-4">
-            {description}
-          </p>
-        )}
-        <button
-          className="mt-4 px-4 py-2 text-slate-700 bg-[#e63746] text-white text-sm font-bold rounded-lg cursor-pointer"
-          onClick={(e) => {}}
-        >
-          {buttonLabel}
-        </button>
+
+        <p className="text-sm text-slate-500 text-center mt-2 px-4 mb-2">
+          {description}
+        </p>
+
+        <input
+          ref={inputRef}
+          type="file"
+          className="hidden"
+          onChange={handleFileChange}
+          accept=".pdf,.xlsx,.csv"
+        />
+        <div className="flex flex-col w-full gap-3">
+          <button
+            className="w-full px-4 py-2.5 bg-slate-100 text-slate-700 dark:text-slate-300 rounded-lg font-bold text-sm hover:bg-slate-200 transition-all flex items-center justify-center gap-2 border border-slate-200 cursor-pointer"
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation()
+              inputRef.current?.click()
+            }}
+          >
+            <FilePlusIcon size={18} className="text-slate-700" />
+            {selectedFile ? 'Archivo Seleccionado: ' : 'Seleccionar Archivo'}
+            <span className="text-[#e63746] font-medium">
+              {selectedFile && selectedFile.name}
+            </span>
+          </button>
+          <button
+            className="w-full px-4 py-2.5 bg-[#e63746] text-white rounded-lg font-bold text-sm shadow-md shadow-[#e63746]/20 hover:bg-[#e63746]/90 transition-all flex items-center justify-center gap-2 cursor-pointer"
+            type="button"
+            onClick={(e) => {
+              e.stopPropagation()
+            }}
+          >
+            <UploadSimpleIcon size={18} className="text-white" />
+            Subir Archivo
+          </button>
+        </div>
       </div>
     </div>
   )
